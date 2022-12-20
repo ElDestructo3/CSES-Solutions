676bfef1cae70da1eca45984cfb30ac6ae447c1b
ElDestructo3 <indiavishal3@gmail.com>
2022-12-20 18:17:29 +0530

    Added more readable solution for Giant Pizza

--- Graph Algorithms/Giant Pizza.cpp
+++ Graph Algorithms/Giant Pizza.cpp
@@ -1,106 +1,128 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+    
 using namespace std;
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define endl "\n"
+const int MOD1 = 1e9 + 7;
+const int MOD2 = 998244353;
+vector<pair<int,int>> dir = {{1,0}, {0,1}, {0,-1}, {-1,0}};
+int MAXN = 3e5 + 5;
+vector<vector<int>>adjList(MAXN), reverseAdjList(MAXN);
+vector<bool> visited(MAXN, false);
+vector<int> topological_order, comp(MAXN);
 
-struct two_sat {
-    int n;
-    vector<vector<int>> g, gr;
-    vector<int> comp, topological_order, answer;
-    vector<bool> vis;
-
-    two_sat() {}
-
-    two_sat(int _n) { init(_n); }
-
-    void init(int _n) {
-        n = _n;
-        g.assign(2 * n, vector<int>());
-        gr.assign(2 * n, vector<int>());
-        comp.resize(2 * n);
-        vis.resize(2 * n);
-        answer.resize(2 * n);
-    }
-
-    void add_edge(int u, int v) {
-        g[u].push_back(v);
-        gr[v].push_back(u);
-    }
-
-    // At least one of them is true
-    void add_clause_or(int i, bool f, int j, bool g) {
-        add_edge(i + (f ? n : 0), j + (g ? 0 : n));
-        add_edge(j + (g ? n : 0), i + (f ? 0 : n));
-    }
-
-    // Only one of them is true
-    void add_clause_xor(int i, bool f, int j, bool g) {
-        add_clause_or(i, f, j, g);
-        add_clause_or(i, !f, j, !g);
-    }
-
-    // Both of them have the same value
-    void add_clause_and(int i, bool f, int j, bool g) {
-        add_clause_xor(i, !f, j, g);
-    }
-
-    void dfs(int u) {
-        vis[u] = true;
-
-        for (const auto &v : g[u])
-            if (!vis[v]) dfs(v);
-
-        topological_order.push_back(u);
-    }
-
-    void scc(int u, int id) {
-        vis[u] = true;
-        comp[u] = id;
-
-        for (const auto &v : gr[u])
-            if (!vis[v]) scc(v, id);
-    }
-
-    bool satisfiable() {
-        fill(vis.begin(), vis.end(), false);
-
-        for (int i = 0; i < 2 * n; i++)
-            if (!vis[i]) dfs(i);
-
-        fill(vis.begin(), vis.end(), false);
-        reverse(topological_order.begin(), topological_order.end());
-
-        int id = 0;
-        for (const auto &v : topological_order)
-            if (!vis[v]) scc(v, id++);
-
-        for (int i = 0; i < n; i++) {
-            if (comp[i] == comp[i + n]) return false;
-            answer[i] = (comp[i] > comp[i + n] ? 1 : 0);
+void dfs(int u) {
+    visited[u] = true;
+    for (const auto &v : adjList[u]) {
+        if (!visited[v]) {
+            //cout << v << endl; 
+            dfs(v);
         }
-
-        return true;
     }
-};
+    topological_order.push_back(u);
+    //cout << u << endl;
+}
 
-signed main() {
-    int N, M;
-    cin >> N >> M;
+void stronglyConnectedComponents(int u, int id) {
+    visited[u] = true;
+    comp[u] = id;
 
-    two_sat solver(M);
+    for (const auto &v : reverseAdjList[u]) {
+        if (!visited[v]) {
+            stronglyConnectedComponents(v, id);
+        }
+    }
+}
 
-    while (N--) {
+
+void solve(int t) {
+    
+    
+    int n, m;
+    cin >> n >> m;
+
+    while (n--) {
         int topping1, topping2;
         char preference1, preference2;
         cin >> preference1 >> topping1 >> preference2 >> topping2;
-
-        solver.add_clause_or(topping1 - 1, preference1 == '+', topping2 - 1, preference2 == '+');
+        bool pref1 = preference1 == '+';
+        bool pref2 = preference2 == '+';
+        // not a to b 
+        int neg_a = topping1 - 1 + (pref1 ? m : 0);
+        int a = topping1 - 1 + (pref1 ? 0 : m);
+        int neg_b = topping2 - 1 + (pref2 ? m : 0);
+        int b = topping2 - 1 + (pref2 ? 0 : m);
+        adjList[neg_a].push_back(b);
+        reverseAdjList[b].push_back(neg_a);
+        // not b to a
+        adjList[neg_b].push_back(a);
+        reverseAdjList[a].push_back(neg_b);
+        
     }
 
-    if (!solver.satisfiable()) {
-        cout << "IMPOSSIBLE";
-        return 0;
+    
+
+    for (int i = 0; i < 2 * m; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
     }
 
-    for (int i = 0; i < M; i++)
-        cout << (solver.answer[i] ? '+' : '-') << " ";
+    fill(visited.begin(), visited.end(), false);
+    reverse(topological_order.begin(), topological_order.end());
+
+    int id = 0;
+    for (const auto &v : topological_order) {
+        if (!visited[v]) {
+            stronglyConnectedComponents(v, id);
+            id++;
+        }
+    }
+    
+    vector<int> ans(m);
+
+    for (int i = 0; i < m; i++) {
+        if (comp[i] == comp[i +m]) {
+            cout << "IMPOSSIBLE" << endl;
+            return;
+        }
+        ans[i] = comp[i] > comp[i + m];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cout << (ans[i] == 1 ? '+' : '-') << " ";
+    }
+
+    
+   
+    
+    
+
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+     
+    
+    int t=1;
+    //cin>>t;
+    
+    for (int i =0; i<t;i++){
+        
+        solve(i + 1);
+    }
+    
+    
+    
 }
